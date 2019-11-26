378a40a46b7b99012924a134bd815da539207493
Brice Friha <brice.friha@aol.fr>
2019-11-26 19:38:59 +0100

    Creation of the model for search result

---
+++ Wikeasy/Wikeasy/Models/SearchResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wikeasy.Models
+{
+    public class SearchResult
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Activities { get; set; }
+        public string Age { get; set; }
+        public string Birthdate { get; set; }
+        public string Birthplace { get; set; }
+        public string Residence { get; set; }
+
+    }
+}

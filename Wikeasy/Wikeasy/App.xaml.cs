beec5a0795cc67083fdd25bd518ef51ce3122b7c
Brice Friha <brice.friha@aol.fr>
2020-01-30 22:38:31 +0100

    Set up themes ressources

--- Wikeasy/Wikeasy/App.xaml.cs
+++ Wikeasy/Wikeasy/App.xaml.cs
@@ -3,18 +3,23 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Wikeasy.Services;
 using Wikeasy.Views;
+using Wikeasy.ViewModels;
+using Wikeasy.Themes;
 
 namespace Wikeasy
 {
     public partial class App : Application
     {
-
+        private static AppViewModel _vm;
         public App()
         {
             InitializeComponent();
 
             //DependencyService.Register<MockDataStore>();
             MainPage = new MainPage();
+
+            _vm = new AppViewModel();
+            BindingContext = _vm;
         }
 
         protected override void OnStart()
@@ -49,9 +54,25 @@ namespace Wikeasy
         /// To handle Light Theme & Dark Theme
         /// </summary>
         /// <param name="theme"></param>
-        void SetTheme(Theme theme)
+        public void SetTheme(Theme theme)
         {
             //Handle Light Theme & Dark Theme
+
+            switch (theme)
+            {
+                // If the system theme is dark
+                case Theme.Dark:
+                    // Switch to dark theme
+                    //_vm.ThemeUrl = "/Themes/DarkTheme.xaml";
+                    App.Current.Resources.MergedDictionaries.Add(new LightTheme());
+                    break;
+                // If the system theme is light
+                case Theme.Light:
+                    // Switch to light theme
+                    //_vm.ThemeUrl = "/Themes/LightTheme.xaml";
+                    App.Current.Resources.MergedDictionaries.Add(new DarkTheme());
+                    break;
+            }
         }
     }
 }
---
+++ Wikeasy/Wikeasy/ViewModels/AppViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wikeasy.ViewModels
+{
+    public class AppViewModel : BaseViewModel
+    {
+        private string _themeUrl;
+        public string ThemeUrl
+        {
+            set
+            {
+                _themeUrl = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return _themeUrl;
+            }
+        }
+
+        public AppViewModel()
+        {
+
+        }
+    }
+}

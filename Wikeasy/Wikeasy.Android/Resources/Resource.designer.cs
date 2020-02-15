278e47201b062b266555affd738ed6adc868d595
Brice Friha <brice.friha@aol.fr>
2020-02-15 16:17:10 +0100

    Rework on things

--- Wikeasy/Wikeasy.Android/Resources/Resource.designer.cs
+++ Wikeasy/Wikeasy.Android/Resources/Resource.designer.cs
@@ -5794,7 +5794,7 @@ namespace Wikeasy.Droid
 			public const int tooltip_frame_light = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int WikeasyIcon = 2131165302;
+			public const int WikeasyLogo = 2131165302;
 			
 			// aapt resource value: 0x7F070077
 			public const int xamarin_logo = 2131165303;
